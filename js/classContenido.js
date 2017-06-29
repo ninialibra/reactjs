596ebd2a2030375e8d63771cf1e65eba962eb7a2
Tania <Tania@iMac.local>
2017-06-29 18:25:29 +0200

    add class

--- js/classContenido.js
+++ js/classContenido.js
@@ -7,3 +7,34 @@ class Encabezado extends React.Component {
     );
   }
 }
+
+class Boton extends React.Component {
+  render() {
+    var color = this.props.color;
+
+    if (!color) { color = 'blue'; }
+
+    return React.createElement('button', {
+      style: { color: color }
+    }, this.props.texto);
+  }
+}
+
+class App extends React.Component {
+  render() {
+    return React.createElement('div', null,
+      React.createElement(
+      	'h1',
+      	{className: 'titulo' + this.props.titulo},
+      	this.props.titulo
+      ),
+      this.props.contenido ?
+        React.createElement(
+      	  Contenido,
+      	  {ancho: 1024, mostrarAds: true},
+          this.props.contenido
+        ) :
+        React.createElement(SinContenido)
+    );
+  }
+}
